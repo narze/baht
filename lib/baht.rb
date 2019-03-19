f9f1be27077e89d1b2a72eaae59f119c2cc29c65
narze <manassarn@gmail.com>
2019-03-19 21:12:12 +0700

    :sparkles: Baht#words

--- lib/baht.rb
+++ lib/baht.rb
@@ -2,5 +2,88 @@ require "baht/version"
 
 module Baht
   class Error < StandardError; end
-  # Your code goes here...
+
+  class << self
+    # Ruby < 2.4 shims for Integer#digits
+    def get_digits(number, base: 10)
+      quotient, remainder = number.divmod(base)
+      quotient == 0 ? [remainder] : [*get_digits(quotient, base: base), remainder]
+    end
+
+    def words(input)
+      out = ""
+      input = input.to_f.round(2)
+      baht = input.truncate
+
+      if input == 0
+        return 'ศูนย์บาทถ้วน'
+      end
+
+      if input < 0
+        out << "ลบ"
+        baht = -baht
+        input = -input
+      end
+
+      baht_million_groups = get_digits(baht, base:1000000)
+
+      sub_hundred = %w{ ศูนย์ หนึ่ง สอง สาม สี่ ห้า หก เจ็ด แปด เก้า
+        สิบ สิบเอ็ด สิบสอง สิบสาม สิบสี่ สิบห้า สิบหก สิบเจ็ด สิบแปด สิบเก้า
+        ยี่สิบ ยี่สิบเอ็ด ยี่สิบสอง ยี่สิบสาม ยี่สิบสี่ ยี่สิบห้า ยี่สิบหก ยี่สิบเจ็ด ยี่สิบแปด ยี่สิบเก้า
+        สามสิบ สามสิบเอ็ด สามสิบสอง สามสิบสาม สามสิบสี่ สามสิบห้า สามสิบหก สามสิบเจ็ด สามสิบแปด สามสิบเก้า
+        สี่สิบ สี่สิบเอ็ด สี่สิบสอง สี่สิบสาม สี่สิบสี่ สี่สิบห้า สี่สิบหก สี่สิบเจ็ด สี่สิบแปด สี่สิบเก้า
+        ห้าสิบ ห้าสิบเอ็ด ห้าสิบสอง ห้าสิบสาม ห้าสิบสี่ ห้าสิบห้า ห้าสิบหก ห้าสิบเจ็ด ห้าสิบแปด ห้าสิบเก้า
+        หกสิบ หกสิบเอ็ด หกสิบสอง หกสิบสาม หกสิบสี่ หกสิบห้า หกสิบหก หกสิบเจ็ด หกสิบแปด หกสิบเก้า
+        เจ็ดสิบ เจ็ดสิบเอ็ด เจ็ดสิบสอง เจ็ดสิบสาม เจ็ดสิบสีี่ เจ็ดสิบห้า เจ็ดสิบหก เจ็ดสิบเจ็ด เจ็ดสิบแปด เจ็ดสิบเก้า
+        แปดสิบ แปดสิบเอ็ด แปดสิบสอง แปดสิบสาม แปดสิบสี่ แปดสิบห้า แปดสิบหก แปดสิบเจ็ด แปดสิบแปด แปดสิบเก้า
+        เก้าสิบ เก้าสิบเอ็ด เก้าสิบสอง เก้าสิบสาม เก้าสิบสี่ เก้าสิบห้า เก้าสิบหก เก้าสิบเจ็ด เก้าสิบแปด เก้าสิบเก้า
+      }
+      sub_ten = sub_hundred.first 10
+
+      if (input < 100)
+        out << sub_hundred[input]
+      else
+        sub_hundred[0] = ""
+
+        digits = %w{ แสน หมื่น พัน ร้อย สิบ }
+        blocks = []
+
+        baht_million_groups.each_with_index do |number, group_index|
+          digit_words = ""
+          hundreds = number / 100
+          hundreds_digits = get_digits(hundreds)
+          len = hundreds_digits.length
+
+          hundreds_digits.each_with_index do |num, index|
+            if num > 0
+              digit_words << sub_ten[num]
+              digit_words << digits[(4 - len + index)]
+            end
+          end
+
+          unless group_index == 0 && baht_million_groups.length > 1 && hundreds == 0
+            sub_hundred[1] = "เอ็ด"
+          end
+
+          digit_words << sub_hundred[number % 100]
+
+          blocks << digit_words
+        end
+
+        out << blocks.join('ล้าน')
+      end
+
+      out << "บาท"
+      satangs = ('%.2f' % input)[-2..-1].to_i
+      if satangs == 0
+        out << "ถ้วน"
+      else
+        sub_hundred[1] = "หนึ่ง"
+        out << sub_hundred[satangs]
+        out << "สตางค์"
+      end
+
+      out.sub(/ศูนย์บาท/, "")
+    end
+  end
 end
--- spec/baht_spec.rb
+++ spec/baht_spec.rb
@@ -3,7 +3,57 @@ RSpec.describe Baht do
     expect(Baht::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  describe '#words' do
+    it "exists" do
+      expect(described_class).to respond_to :words
+    end
+
+    shared_examples "translatable" do |from, to|
+      it "translates #{from} to #{to}" do
+        expect(described_class.words(from)).to eq to
+      end
+    end
+
+    it_behaves_like "translatable", nil, "ศูนย์บาทถ้วน"
+    it_behaves_like "translatable", 0, "ศูนย์บาทถ้วน"
+    it_behaves_like "translatable", 1, "หนึ่งบาทถ้วน"
+    it_behaves_like "translatable", 2, "สองบาทถ้วน"
+    it_behaves_like "translatable", 3, "สามบาทถ้วน"
+    it_behaves_like "translatable", 4, "สี่บาทถ้วน"
+    it_behaves_like "translatable", 5, "ห้าบาทถ้วน"
+    it_behaves_like "translatable", 6, "หกบาทถ้วน"
+    it_behaves_like "translatable", 7, "เจ็ดบาทถ้วน"
+    it_behaves_like "translatable", 8, "แปดบาทถ้วน"
+    it_behaves_like "translatable", 9, "เก้าบาทถ้วน"
+    it_behaves_like "translatable", 10, "สิบบาทถ้วน"
+    it_behaves_like "translatable", 100, "หนึ่งร้อยบาทถ้วน"
+    it_behaves_like "translatable", 101, "หนึ่งร้อยเอ็ดบาทถ้วน"
+    it_behaves_like "translatable", 110, "หนึ่งร้อยสิบบาทถ้วน"
+    it_behaves_like "translatable", 111, "หนึ่งร้อยสิบเอ็ดบาทถ้วน"
+    it_behaves_like "translatable", 200, "สองร้อยบาทถ้วน"
+    it_behaves_like "translatable", 201, "สองร้อยเอ็ดบาทถ้วน"
+    it_behaves_like "translatable", 1000, "หนึ่งพันบาทถ้วน"
+    it_behaves_like "translatable", 10000, "หนึ่งหมื่นบาทถ้วน"
+    it_behaves_like "translatable", 100000, "หนึ่งแสนบาทถ้วน"
+    it_behaves_like "translatable", 1000000, "หนึ่งล้านบาทถ้วน"
+    it_behaves_like "translatable", 2000001, "สองล้านเอ็ดบาทถ้วน"
+    it_behaves_like "translatable", 10000000, "สิบล้านบาทถ้วน"
+    it_behaves_like "translatable", 21000000, "ยี่สิบเอ็ดล้านบาทถ้วน"
+    it_behaves_like "translatable", 9_000_000_000, "เก้าพันล้านบาทถ้วน"
+    it_behaves_like "translatable", 1_000_000_000_000, "หนึ่งล้านล้านบาทถ้วน"
+    it_behaves_like "translatable", 1234567890123, "หนึ่งล้านสองแสนสามหมื่นสี่พันห้าร้อยหกสิบเจ็ดล้านแปดแสนเก้าหมื่นหนึ่งร้อยยี่สิบสามบาทถ้วน"
+
+    # Decimals
+    it_behaves_like "translatable", 1.25, "หนึ่งบาทยี่สิบห้าสตางค์"
+    it_behaves_like "translatable", 100.019, "หนึ่งร้อยบาทสองสตางค์"
+    it_behaves_like "translatable", 20001000001.1, "สองหมื่นเอ็ดล้านเอ็ดบาทสิบสตางค์"
+    it_behaves_like "translatable", 100.0, "หนึ่งร้อยบาทถ้วน"
+    it_behaves_like "translatable", 0.01, "หนึ่งสตางค์"
+
+    # Negatives
+    it_behaves_like "translatable", -20000000, "ลบยี่สิบล้านบาทถ้วน"
+    it_behaves_like "translatable", -0.001, "ศูนย์บาทถ้วน"
+    it_behaves_like "translatable", -0.01, "ลบหนึ่งสตางค์"
+    it_behaves_like "translatable", -3.5, "ลบสามบาทห้าสิบสตางค์"
   end
 end
